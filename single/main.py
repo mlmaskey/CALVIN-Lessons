1c2a353c03c33674b7862f82c7a8c2a7d68f2f4a
Mahesh Lal Maskey <45472541+mlmaskey@users.noreply.github.com>
2020-08-03 17:21:21 -0700

    Update main.py

--- single/main.py
+++ single/main.py
@@ -2,7 +2,7 @@ from calvin import *
 #from postprocessor import *
 
 # specify data file
-calvin = CALVIN('calvin/data/dataRecharge2.csv')
+calvin = CALVIN('calvin/data/links_infeasible.csv')
 
 # create pyomo model from specified data file
 calvin.create_pyomo_model(debug_mode=False)
@@ -11,4 +11,4 @@ calvin.create_pyomo_model(debug_mode=False)
 calvin.solve_pyomo_model(solver='glpk', nproc=10, debug_mode=False)
 
 # postprocess results to create time-series files
-postprocess(calvin.df, calvin.model, resultdir='linksPerfect')
+postprocess(calvin.df, calvin.model, resultdir='result')
