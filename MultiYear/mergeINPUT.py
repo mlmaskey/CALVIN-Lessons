993e5ab68974a9755638a691a1597dc0fd0668b4
Mahesh Lal Maskey <45472541+mlmaskey@users.noreply.github.com>
2020-08-03 17:18:09 -0700

    Update mergeINPUT.py

--- MultiYear/mergeINPUT.py
+++ MultiYear/mergeINPUT.py
@@ -2,15 +2,16 @@
 """
 Created on Mon Jul  6 22:43:02 2020
 
-@author: Water Management Lab
+@author: Mahesh L Maskey
+Description: Reads the input from limited foresight runs for perfect forsight
 """
 
 import pandas as pd
 dataMerge = pd.DataFrame()
 for i in range(1922,2004):
   print('\nNow running WY %d' % i)
-  fileName = 'calvin/data/dataRecharge2/linksWY%d-final.csv' % i
+  fileName = 'calvin/data/baseData/linksWY%d-final.csv' % i
   F = pd.read_csv(fileName, index_col=0, parse_dates=True)
   dataMerge = pd.concat([dataMerge, F], axis = 0)
   
-dataMerge.to_csv('Single/calvin/data/dataRecharge2.csv', index=True)
+dataMerge.to_csv('Single/calvin/data/linkPerfect.csv', index=True)
